94c88778f1aaf4f9b8b9d2f443859520b6436850
Marcos126 <natalemarcos06@gmail.com>
2024-06-13 13:46:13 -0300

    empezando uwu

---
+++ HelloWorld/src/main/java/com/mycompany/App.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "\n Hello World\n Esto es una prueba\n zurdos de mierda hoy la van a pasar para el orto");
+    }
+}
