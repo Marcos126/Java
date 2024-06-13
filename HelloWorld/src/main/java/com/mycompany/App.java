ec8b599fc9a9ce25e2e559ab961c70d5f5fefd53
Marcos126 <natalemarcos06@gmail.com>
2024-06-13 18:45:34 -0300

    empezando uwu

--- HelloWorld/src/main/java/com/mycompany/App.java
+++ HelloWorld/src/main/java/com/mycompany/App.java
@@ -9,6 +9,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "\n Hello World\n Esto es una prueba\n zurdos de mierda hoy la van a pasar para el orto");
+        //tipos de variables
+        int numeros = 123;
+        char caracter = '1';
+        String cadenas = "VLLC";
+        boolean siono = true;
+        float pi = 3.14f;
+        System.out.println(
+            "\n\n Esto es un int: " + numeros + "\n\n" +
+            "\n\n Esto es un char: " + caracter + "\n\n" +
+            "\n\n Esto es un String: " + cadenas + "\n\n" +
+            "\n\n Esto es un boolean: " + siono + "\n\n" +
+            "\n\n Esto es un float: " + pi + "\n\n" 
+        );
+        
     }
+
 }
