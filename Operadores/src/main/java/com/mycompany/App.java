0798f48f99b44ae1dd7fc5d1647edca123768e39
Marcos126 <natalemarcos06@gmail.com>
2024-06-13 19:49:40 -0300

    empezando uwu

--- Operadores/src/main/java/com/mycompany/App.java
+++ Operadores/src/main/java/com/mycompany/App.java
@@ -1,5 +1,7 @@
 package com.mycompany;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner input = new Scanner(System.in);
+        float numero1,numero2,suma,resta,div,mult,resto;
+
+        System.out.println("Por favor ingrese 2 numeros.");
+        numero1 = input.nextFloat();
+        numero2 = input.nextFloat();
+
+        suma = numero1+numero2;
+        resta = numero1-numero2;
+        div = numero1/numero2;
+        mult = numero1 * numero2;
+        resto = numero1%numero2;
+
+        System.out.println(" Resultado de la suma: " + suma);
+        System.out.println(" Resultado de la resta: " + resta);
+        System.out.println(" Resultado de la division: " + div );
+        System.out.println(" Resultado de la multiplicacion: " + mult);
+        System.out.println(" Resultado del resto: " + resto);
     }
+
+
 }
