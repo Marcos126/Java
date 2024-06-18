6b1165a7094a2f7e4d2cbdfb767671af924cd066
Marcos126 <natalemarcos06@gmail.com>
2024-06-18 15:28:22 -0300

    tarea

--- ejercicios/8-ecuacionesDeSegundoGrado/src/main/java/com/marquitos/App.java
+++ ejercicios/8-ecuacionesDeSegundoGrado/src/main/java/com/marquitos/App.java
@@ -10,19 +10,34 @@ public class App
 {
     public static void main( String[] args )
     {
-        double a,b,c,x1,x2;
+        double a,b,c,x1,x2,discriminante;
+
         Scanner input = new Scanner(System.in);
         System.out.println( "Por favor ingrese valor de a" );
-        a = input.nextFloat();
+        a = input.nextDouble();
         System.out.println( "Por favor ingrese valor de b" );
-        b = input.nextFloat();
+        b = input.nextDouble();
         System.out.println( "Por favor ingrese valor de c" );
-        c = input.nextFloat();
+        c = input.nextDouble();
 
-        x1 = (-b + Math.sqrt(Math.pow(b,2)- 4*a*c)) /2*a;
-        x2 = (-b - Math.sqrt(Math.pow(b,2)- 4*a*c)) /2*a;
+        discriminante = Math.pow(b,2)- 4*a*c;
+
         
-        System.out.println(x1 + "\n" + x2 );
+        if (discriminante >= 0){
+
+        x1 = (-b + Math.sqrt(discriminante)) / (2*a);
+        x2 = (-b - Math.sqrt(discriminante)) / (2*a);
+
+        System.out.println("\nX1 = "+ x2 + "\nX2 = " + x1 );
+
+        }
+
+        else {
+        System.out.println("La cuenta no tiene solucion");
+
+        }
+
+        
         input.close();
         //ax^2 + bx + c = 0
         //x = (-b +/- sqrt(b^2 )-4ac)/2a
