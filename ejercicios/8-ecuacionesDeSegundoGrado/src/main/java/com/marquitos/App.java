6da28e934f4167fbfb051a90da3c34779ed4fa1c
Marcos126 <natalemarcos06@gmail.com>
2024-06-18 13:41:07 -0300

    tarea

--- ejercicios/8-ecuacionesDeSegundoGrado/src/main/java/com/marquitos/App.java
+++ ejercicios/8-ecuacionesDeSegundoGrado/src/main/java/com/marquitos/App.java
@@ -1,5 +1,7 @@
 package com.marquitos;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        double a,b,c,x1,x2;
+        Scanner input = new Scanner(System.in);
+        System.out.println( "Por favor ingrese valor de a" );
+        a = input.nextFloat();
+        System.out.println( "Por favor ingrese valor de b" );
+        b = input.nextFloat();
+        System.out.println( "Por favor ingrese valor de c" );
+        c = input.nextFloat();
+
+        x1 = (-b + Math.sqrt(Math.pow(b,2)- 4*a*c)) /2*a;
+        x2 = (-b - Math.sqrt(Math.pow(b,2)- 4*a*c)) /2*a;
+        
+        System.out.println(x1 + "\n" + x2 );
+        input.close();
+        //ax^2 + bx + c = 0
+        //x = (-b +/- sqrt(b^2 )-4ac)/2a
+        // x = -b + Math.sqrt(Math.pow(b,2) - 4*a*c)/2*a
+        // x = -b - Math.sqrt(Math.pow(b,2) - 4*a*c)/2*a
+
     }
 }
